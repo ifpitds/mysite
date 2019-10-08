98126ea6097ba22e64cc4cb44f991b249e783029
ifpitds <root@localhost.localdomain>
2019-10-08 17:50:11 -0300

    update

--- core/views.py
+++ core/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-   return HttpResponse("hrllo")
+   return HttpResponse("Use https://ifpitds.pythonanywhere.com/admin para acessar as aplicações")
